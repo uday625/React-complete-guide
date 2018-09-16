03a515c879fbb6ebb45b1856adb4ce9f3197675a
UDAY BAG <uday625@Nemesis-2.local>
2018-09-15 20:21:02 -0400

    removes Error Boundary

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,11 +4,6 @@ import classes from './Person.css'
 
 const person = (props) =>{
 
-    const rnd = Math.random();
-    if (rnd >0.7){
-        throw new Error ("Something went wrong!");
-    }
-
     return(
         <div className={classes.person} >
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
